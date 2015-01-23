3b2a6404c6f5f31c8bda7aa3c8edad2d3e260caf
bene200 <berau90@gmail.com>
2015-01-23 12:51:35 +0100

    removed outdated example

--- examples/first.js
+++
@@ -1,7 +0,0 @@
-var rna = require("drawrnajs");
-var app = rna.vis;
-
-var input = ["AAAAU", "(...)"];
-var struct = rna.t.transformDotBracket(input[0], input[1]);
-yourDiv.className += " cy";
-app({graph: struct, el: yourDiv});
